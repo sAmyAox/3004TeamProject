40108700b68fa397f5f4823e28b717906e8b321b
Cube <weidatseng18@gmail.com>
2023-11-29 11:00:32 -0500

    update source file

---
+++ source code/header/device.h
@@ -0,0 +1,14 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+
+class device
+{
+private:
+    
+public:
+    device(/* args */);
+    ~device();
+};
+
+#endif
---
+++ source code/header/patient.h
@@ -0,0 +1,16 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+class patient
+{
+private:
+    
+
+
+public:
+    patient(/* args */);
+    ~patient();
+};
+
+
+#endif
