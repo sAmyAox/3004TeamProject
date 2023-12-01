09a4933a58c89bd34ec8cfd3dbe0604536ee809a
sAmyAox <101735957+sAmyAox@users.noreply.github.com>
2023-11-30 19:09:18 -0500

    Add files via upload

---
+++ source code/AEDplus/AEDplus/device.h
@@ -0,0 +1,38 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <QObject>
+class device:public QObject
+{
+    Q_OBJECT
+
+private:
+    int battery;
+    bool operational;//if =false(didn;t press on), no respose, check before each function?
+    bool shockable;//if shockable==true, then wait for the button_shock,else display prompt, ask for CPR
+
+
+public:
+    device(/* args */);
+    ~device();
+
+    void get_patient_status();//get patient's heart rate and vf.
+    void shock();//deliver shock to patient
+    void display_device_status();//show status when the device turned on
+    void detect_rhythm();//getter for patient's heart rhythm
+    void workflow();
+    void display_CPR_feedback();//when pressed button, tell user if it is bad or good.
+    //void error_mode();//if the device wait for too long, enter error mode.(asking to give CPR)
+    void display_prompt();//maybe qstring?
+
+
+signals:
+
+public slots:
+
+
+
+
+};
+
+#endif
